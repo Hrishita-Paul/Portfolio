8b68c630829742100106a336304cb1cd13e08c7d
Hrishita Paul <hrishita.paul03@gmail.com>
2024-05-11 18:35:01 -0700

    New commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer(){
                 <div className="rightColumn">
                 <p className="footer-link"><a href="https://github.com/Hrishita-Paul"> Github </a></p>
                 <p className="footer-link"><a href="https://www.geeksforgeeks.org/user/hrishit4kfx/"  target="_blank"> GeeksForGeeks profile</a></p>
-                <p className="footer-link">Resume link: <a href="https://drive.google.com/file/d/11dNF6sDjEzsbY8HfzLbI8ws83Yaq38AM/view?usp=sharing">Click Here</a></p>
+                <p className="footer-link">Resume link: <a href="https://drive.google.com/file/d/11dNF6sDjEzsbY8HfzLbI8ws83Yaq38AM/view?usp=sharing" target="_blank">Click Here</a></p>
                 </div>
         </div>
     )
